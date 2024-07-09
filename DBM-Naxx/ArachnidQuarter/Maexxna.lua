9cac18c916f503397d1f5a03ecfa0b5b8c522d4e
broizter <ragu.alvor-0m@icloud.com>
2024-07-09 12:27:14 +0000

    Naxx-Maexxna: Adjusted timers

--- DBM-Naxx/ArachnidQuarter/Maexxna.lua
+++ DBM-Naxx/ArachnidQuarter/Maexxna.lua
@@ -19,26 +19,26 @@ local warnSpidersNow	= mod:NewAnnounce("WarningSpidersNow", 4, 17332)
 local specWarnWebWrap	= mod:NewSpecialWarningSwitch(28622, "RangedDps", nil, nil, 1, 2)
 local yellWebWrap		= mod:NewYellMe(28622)
 
-local timerWebSpray		= mod:NewNextTimer(40, 29484, nil, nil, nil, 2)
-local timerWebWrap		= mod:NewNextTimer(20, 28622, nil, "RangedDps|Healer", nil, 3)
+local timerWebSpray		= mod:NewNextTimer(30, 29484, nil, nil, nil, 2)
+local timerWebWrap		= mod:NewNextTimer(30, 28622, nil, "RangedDps|Healer", nil, 3)
 local timerSpider		= mod:NewTimer(40, "TimerSpider", 17332, nil, nil, 1)
 
 local function Spiderlings(self)
-	warnSpidersSoon:Schedule(35)
-	warnSpidersNow:Schedule(40)
-	timerSpider:Start(40)
+	warnSpidersSoon:Schedule(25)
+	warnSpidersNow:Schedule(30)
+	timerSpider:Start(30)
 	self:Unschedule(Spiderlings)
-	self:Schedule(40, Spiderlings, self)
+	self:Schedule(30, Spiderlings, self)
 end
 
 function mod:OnCombatStart(delay)
-	warnWebSpraySoon:Schedule(35 - delay)
-	timerWebSpray:Start(40 - delay)
-	timerWebWrap:Start(20 - delay)
+	warnWebSpraySoon:Schedule(30 - delay)
+	timerWebSpray:Start(35 - delay)
+	timerWebWrap:Start(15 - delay)
 	warnSpidersSoon:Schedule(25 - delay)
 	warnSpidersNow:Schedule(30 - delay)
 	timerSpider:Start(30 - delay)
-	self:Schedule(30 - delay, Spiderlings, self)
+	self:Schedule(25 - delay, Spiderlings, self)
 end
 
 function mod:OnCombatEnd(wipe)
@@ -68,7 +68,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 	elseif args:IsSpellID(29484, 54125) and self:AntiSpam(3, 3) then -- Web Spray
 		warnWebSprayNow:Show()
-		warnWebSpraySoon:Schedule(35)
+		warnWebSpraySoon:Schedule(25)
 		timerWebSpray:Start()
 	end
 end
