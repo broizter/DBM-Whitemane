759fb899c69b3e8437113bcaeb1a5938d7c74488
broizter <ragu.alvor-0m@icloud.com>
2024-07-26 20:16:10 +0200

    SW-Brutallus: Always show range 12y

--- DBM-Sunwell/Brutallus.lua
+++ DBM-Sunwell/Brutallus.lua
@@ -34,9 +34,8 @@ local timerBurnCD		= mod:NewCDTimer(60, 46394, nil, nil, nil, 3)
 local berserkTimer		= mod:NewBerserkTimer(mod:IsTimewalking() and 300 or 360)
 
 mod:AddSetIconOption("BurnIcon", 46394, true, false, {1, 2, 3, 4, 5, 6, 7, 8})
-mod:AddRangeFrameOption(4, 46394)
+mod:AddRangeFrameOption("12")
 mod:AddMiscLine(DBM_CORE_L.OPTION_CATEGORY_DROPDOWNS)
-mod:AddDropdownOption("RangeFrameActivation", {"AlwaysOn", "OnDebuff"}, "OnDebuff", "misc")
 
 mod.vb.burnIcon = 8
 local debuffName = DBM:GetSpellInfo(46394)
@@ -54,8 +53,8 @@ function mod:OnCombatStart(delay)
 	timerMeteorCD:Start(12-delay)
 	timerStompCD:Start(-delay)
 	berserkTimer:Start(-delay)
-	if self.Options.RangeFrame and self.Options.RangeFrameActivation == "AlwaysOn" then
-		DBM.RangeCheck:Show(4)
+	if self.Options.RangeFrame then
+		DBM.RangeCheck:Show(12)
 	end
 end
 
@@ -84,13 +83,6 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnBurn:Show()
 			specWarnBurn:Play("targetyou")
 			yellBurn:Yell()
-		end
-		if self.Options.RangeFrame and self.Options.RangeFrameActivation == "OnDebuff" then
-			if DBM:UnitDebuff("player", args.spellName) then--You have debuff, show everyone
-				DBM.RangeCheck:Show(4, nil)
-			else--You do not have debuff, only show players who do
-				DBM.RangeCheck:Show(4, DebuffFilter)
-			end
 		end
 	elseif args.spellId == 45185 then
 		if args:IsPlayer() then
