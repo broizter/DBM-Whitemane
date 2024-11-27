6aeed9cc669ffc43df63542d6c127332415f0505
broizter <ragu.alvor-0m@icloud.com>
2024-11-27 13:56:24 +0100

    VoA-Koralon: Meteor fist fixes

--- DBM-VoA/Koralon.lua
+++ DBM-VoA/Koralon.lua
@@ -7,9 +7,9 @@ mod:SetCreatureID(35013)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 67328 66665 66725 68161",
+	"SPELL_CAST_START 67328 66665",
 	"SPELL_AURA_APPLIED 66684 67332 66721",
-	"SPELL_AURA_APPLIED_DOSE 66721"
+	"SPELL_AURA_APPLIED_DOSE 66721 66725 68161"
 )
 
 local warnBreath			= mod:NewSpellAnnounce(66665, 3)
@@ -19,7 +19,7 @@ local warnBurningFury		= mod:NewStackAnnounce(66721, 2, nil, "Tank|Healer")
 
 local specWarnCinder		= mod:NewSpecialWarningMove(66684, nil, nil, nil, 1, 2)
 
-local timerNextMeteor		= mod:NewNextTimer(47, 66725, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON) -- REVIEW! variance? (10N Lordaeron 2022/09/23) - 47.0
+local timerNextMeteor		= mod:NewNextTimer(45, 66725, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON) -- REVIEW! variance? (10N Lordaeron 2022/09/23) - 47.0
 local timerNextBurningFury	= mod:NewNextTimer(20, 66721, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.HEALER_ICON) -- (10N Lordaeron 2022/09/23) - 20.0, 22.0, 20.0, 20.0
 local timerBreath			= mod:NewBuffActiveTimer(4.5, 66665, nil, nil, nil, 2)
 local timerBreathCD			= mod:NewCDTimer(45, 66665, nil, nil, nil, 2) -- (10N Lordaeron 2022/09/23) - 45.0, 45.0
@@ -28,7 +28,7 @@ local timerKoralonEnrage	= mod:NewBerserkTimer(300, nil, "KoralonEnrage")
 
 function mod:OnCombatStart(delay)
 	timerKoralonEnrage:Start(-delay)
-	timerNextMeteor:Start(44.9-delay) -- REVIEW! variance? (10N Lordaeron 2022/09/23) - pull:44.9
+	timerNextMeteor:Start(30-delay) -- REVIEW! variance? (10N Lordaeron 2022/09/23) - pull:44.9
 	warnMeteorSoon:Schedule(44.9-5-delay)
 	timerBreathCD:Start(9-delay)
 	timerNextBurningFury:Start(19-delay)
@@ -39,10 +39,6 @@ function mod:SPELL_CAST_START(args)
 		warnBreath:Show()
 		timerBreath:Start()
 		timerBreathCD:Start()
-	elseif args:IsSpellID(66725, 68161) then
-		warnMeteor:Show()
-		timerNextMeteor:Start()
-		warnMeteorSoon:Schedule(42)
 	end
 end
 
@@ -53,6 +49,10 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif args.spellId == 66721 then
 		warnBurningFury:Show(args.destName, args.amount or 1)
 		timerNextBurningFury:Start()
+	elseif args:IsSpellID(66725, 68161) then -- Meteor first
+		warnMeteor:Show()
+		timerNextMeteor:Start()
+		warnMeteorSoon:Schedule(42)
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
