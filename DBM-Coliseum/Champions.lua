36e259f8fc7b5cf9449d9cf796bb77dfe9c38d6a
broizter <ragu.alvor-0m@icloud.com>
2024-11-27 14:56:58 +0100

    ToC-Champions: Only show eyebeam timer on heroic

--- DBM-Coliseum/Champions.lua
+++ DBM-Coliseum/Champions.lua
@@ -152,7 +152,9 @@ local timerBloodlustCD		= mod:NewCDTimer(300, 65980)
 local timerSpawnTimer		= mod:NewTimer(33, "TimerSpawnTimer", 47436, nil, nil, 6)
 
 function mod:OnCombatStart(delay)
-	timerEyebeam:Start(25-delay)
+	if self:IsDifficulty("heroic10", "heroic25") then
+		timerEyebeam:Start(25-delay)
+	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
