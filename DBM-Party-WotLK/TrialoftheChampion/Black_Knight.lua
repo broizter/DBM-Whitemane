a23caf1a1f81c85f6496fbb92e728f457324f8ba
broizter <ragu.alvor-0m@icloud.com>
2024-11-28 13:33:36 +0100

    ToC5: Make code more readable

--- DBM-Party-WotLK/TrialoftheChampion/Black_Knight.lua
+++ DBM-Party-WotLK/TrialoftheChampion/Black_Knight.lua
@@ -2,7 +2,7 @@ local mod	= DBM:NewMod("BlackKnight", "DBM-Party-WotLK", 13)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("20220518110528")
-mod:SetCreatureID(35451, 10000)		-- work around, DBM API failes to handle a Boss to die, rebirth, die again, rebirth again and die to loot...
+mod:SetCreatureID(35451, 10000)	-- work around, DBM API failes to handle a Boss to die, rebirth, die again, rebirth again and die to loot...
 mod:SetUsedIcons(8)
 
 mod:RegisterCombat("combat")
@@ -19,14 +19,14 @@ mod:RegisterEventsInCombat(
 	"SPELL_MISSED 67781 67876 67729 67886"
 )
 
-local warnMarked			= mod:NewTargetNoFilterAnnounce(67823, 3)
+local warnMarked		= mod:NewTargetNoFilterAnnounce(67823, 3)
 
 local specWarnDesecration	= mod:NewSpecialWarningMove(67781, nil, nil, nil, 1, 8)
 local specWarnExplode		= mod:NewSpecialWarningRun(67751, "Melee", nil, 2, 4, 2)
 
 local timerCombatStart		= mod:NewCombatTimer(38.5)
-local timerMarked			= mod:NewTargetTimer(10, 67823, nil, nil, nil, 3)
-local timerExplode			= mod:NewCastTimer(4, 67729, nil, nil, nil, 2)
+local timerMarked		= mod:NewTargetTimer(10, 67823, nil, nil, nil, 3)
+local timerExplode		= mod:NewCastTimer(4, 67729, nil, nil, nil, 2)
 
 mod:AddSetIconOption("SetIconOnMarkedTarget", 67823, false, false, {8})
 mod:AddBoolOption("AchievementCheck", false, "announce")
@@ -38,7 +38,7 @@ function mod:OnCombatStart()
 end
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(67729, 67886) and self:AntiSpam(2, 2) then							-- Explode (elite explodes self, not BK. Phase 2)
+	if args:IsSpellID(67729, 67886) and self:AntiSpam(2, 2) then -- Explode (elite explodes self, not BK. Phase 2)
 		specWarnExplode:Show()
 		specWarnExplode:Play("justrun")
 		timerExplode:Start()
@@ -46,7 +46,7 @@ function mod:SPELL_CAST_START(args)
 end
 
 function mod:SPELL_DAMAGE(_, _, _, destGUID, destName, _, spellId)
-	if (spellId == 67781 or spellId == 67876) and destGUID == UnitGUID("player") and self:AntiSpam(3, 1) then		-- Desecration
+	if (spellId == 67781 or spellId == 67876) and destGUID == UnitGUID("player") and self:AntiSpam(3, 1) then -- Desecration
 		specWarnDesecration:Show()
 		specWarnDesecration:Play("watchfeet")
 	elseif spellId == 67729 or spellId == 67886 then
@@ -59,13 +59,13 @@ end
 mod.SPELL_MISSED = mod.SPELL_DAMAGE
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(67823, 67882) and args:IsDestTypePlayer() then							-- Marked For Death
+	if args:IsSpellID(67823, 67882) and args:IsDestTypePlayer() then -- Marked For Death
 		if self.Options.SetIconOnMarkedTarget then
 			self:SetIcon(args.destName, 8, 10)
 		end
 		warnMarked:Show(args.destName)
 		timerMarked:Show(args.destName)
-	elseif args.spellId == 67751 and self:AntiSpam(2, 2) then	-- Ghoul Explode (BK exlodes Army of the dead. Phase 3)
+	elseif args.spellId == 67751 and self:AntiSpam(2, 2) then -- Ghoul Explode (BK exlodes Army of the dead. Phase 3)
 		specWarnExplode:Show()
 		specWarnExplode:Play("justrun")
 	end
