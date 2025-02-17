eef3dd8182b3d84dd5853789b2751e35ba33cea7
broizter <ragu.alvor-0m@icloud.com>
2025-02-17 13:31:58 +0100

    ICC-Saurfang: Adjust alliance combat start timer

--- DBM-Icecrown/TheLowerSpire/Deathbringer.lua
+++ DBM-Icecrown/TheLowerSpire/Deathbringer.lua
@@ -27,7 +27,7 @@ mod:RegisterEventsInCombat(
 local myRealm = select(3, DBM:GetMyPlayerInfo())
 
 -- General
-local timerCombatStart		= mod:NewCombatTimer(47.10)
+local timerCombatStart		= mod:NewCombatTimer(45)
 local enrageTimer			= mod:NewBerserkTimer((myRealm == "Lordaeron" or myRealm == "Frostmourne") and 420 or 480)
 
 mod:RemoveOption("HealthFrame")
