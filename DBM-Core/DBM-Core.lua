9b48f110a210ce57228d49bf9de27e015b41f794
broizter <ragu.alvor-0m@icloud.com>
2025-02-23 14:04:04 +0100

    DBM-Core: Bump revision timestamp

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,7 +82,7 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20250220124759"),
+	Revision = parseCurseDate("20250223140324"),
 	DisplayVersion = "11.5.1 alpha", -- the string that is shown as version
 	ReleaseRevision = releaseDate(2025, 02, 20) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
