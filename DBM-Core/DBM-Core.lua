619b2e6da971b68d4d5ebc020db2e8f6718dd6f9
broizter <ragu.alvor-0m@icloud.com>
2025-02-23 14:47:26 +0100

    DBM-Core: Start 11.5.1 alpha2 cycle

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,8 +82,8 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20250223140324"),
-	DisplayVersion = "11.5.1 alpha", -- the string that is shown as version
+	Revision = parseCurseDate("20250223144735"),
+	DisplayVersion = "11.5.1 alpha2", -- the string that is shown as version
 	ReleaseRevision = releaseDate(2025, 02, 20) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
